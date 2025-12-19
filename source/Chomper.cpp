7a02dea82c468f0046b1cf300f7165b07e29b6a4
NachoErmisch <nachoermisch@gmail.com>
2025-12-19 23:55:59 +0100

    chomper

---
+++ source/Chomper.cpp
@@ -0,0 +1,35 @@
+#include "Chomper.h"
+#include "TimeManager.h"
+#include <cmath>
+Chomper::Chomper(Vector2 spawnPosition, int indexInLine)
+    : Enemy()
+{
+    _renderer = new ImageRenderer(_transform, "resources/image.png", Vector2(0.f, 0.f), Vector2(0.f, 0.f));
+    _transform->size = Vector2(100.f, 100.f);
+    _transform->position = spawnPosition;
+    lineCenter = spawnPosition;
+    radius = 30.f;
+    angularSpeed = 2.5f;
+    angle = indexInLine * 0.5f; // Offset each chomper
+    leftDrift = 50.f;
+    _physics->AddCollider(new AABB(_transform->position, _transform->size));
+    currentState = CIRCLE_MOVE;
+    health = 2;
+}
+void Chomper::Behaviour()
+{
+    if (currentState == CIRCLE_MOVE)
+    {
+        angle += angularSpeed * TM.GetDeltaTime();
+        // Move line center to the left
+        lineCenter.x -= leftDrift * TM.GetDeltaTime();
+        // Circle around the moving center
+        _transform->position.x = lineCenter.x + cos(angle) * radius;
+        _transform->position.y = lineCenter.y + sin(angle) * radius;
+    }
+}
+void Chomper::Update()
+{
+    Enemy::Update();
+    Behaviour();
+}
---
+++ source/Chomper.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Enemy.h"
+class Chomper : public Enemy
+{
+private:
+    Vector2 lineCenter;
+    float angle;
+    float radius;
+    float angularSpeed;
+    float leftDrift;
+public:
+    Chomper(Vector2 spawnPosition, int indexInLine);
+    void Update() override;
+    void Behaviour() override;
+};
