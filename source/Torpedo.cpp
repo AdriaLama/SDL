46133c70965e91e0055c0fd1c882347da848c69f
Adria Lama <adria.lama@enti.cat>
2025-12-20 22:58:57 +0100

    missiles+ torpedo changes

--- source/Torpedo.cpp
+++ source/Torpedo.cpp
@@ -1,9 +1,24 @@
 #include "Torpedo.h"
+#include "GameManager.h"
 
 void Torpedo::Behaviour()
 {
-	_physics->AddForce(Vector2(-velocityX, 0.f));
-	_physics->SetLinearDrag(10.f);
+    Player* player = GAME_MANAGER.GetPlayer();
+
+    _physics->AddForce(Vector2(-900.f, 0.f));
+    _physics->SetLinearDrag(10.f);
+    if (player != nullptr)
+    {
+        Vector2 playerPos = player->GetTransform()->position;
+        float distance = sqrt(pow(playerPos.x - _transform->position.x, 2));
+
+        if (distance < 1000.f)
+        {
+            _physics->AddForce(Vector2(-velocityX, 0.f));
+            _physics->SetLinearDrag(10.f);
+        }
+    }
+
 
 }
 
