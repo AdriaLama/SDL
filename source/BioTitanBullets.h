845cdb5b9f2d1bb64256e23c0c163106df71cfe0
Adria Lama <adria.lama@enti.cat>
2025-12-21 16:04:36 +0100

    bioTitan

---
+++ source/BioTitanBullets.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "TimeManager.h"
+#include "RenderManager.h"
+#include "Enemy.h"
+
+class BioTitanBullets : public Enemy
+{
+public:
+    BioTitanBullets(Vector2 spawnPosition) : Enemy()
+    {
+        _renderer = new ImageRenderer(_transform, "resources/image.png", Vector2(0.f, 0.f), Vector2(0.f, 0.f));
+        _transform->position = spawnPosition;
+        _transform->size = Vector2(75.f, 75.f);
+    }
+
+    void Update() override
+    {
+       Enemy::Update();
+
+       _physics->AddForce(Vector2(-3500.f, 0.f));
+       _physics->SetLinearDrag(10.f);
+
+        if (_transform->position.x < -100.f || _transform->position.x > RM->WINDOW_WIDTH + 100.f || _transform->position.y < -100.f || _transform->position.y > RM->WINDOW_HEIGHT + 100.f)
+        {
+            Destroy();
+        }
+    }
+
+    void Behaviour() override {}
+};
